788db454021794d4647dff15fff83d20f3e90c70
temur shamshidinov <temur.software.dev.go@gmail.com>
2024-09-19 17:09:23 +0500

    repoi/repoi.go

--- storage/repoI/repoi.go
+++ storage/repoI/repoi.go
@@ -22,4 +22,34 @@ type CourseRepoI interface {
 	DeleteCourse(ctx context.Context, id string) error
 }
 
+type GroupRepoI interface {
+	CreateGroup(ctx context.Context, req *models.Group) error
+	GetGroupList(ctx context.Context, req *models.GetGroupsList) (*models.GetGroupsList, error)
+	GetGroupByID(ctx context.Context, id string) (*models.Group, error)
+	UpdateGroup(ctx context.Context, req *models.Group) error
+	DeleteGroup(ctx context.Context, id string) error
+}
 
+type SubjectRepoI interface {
+	CreateSubject(ctx context.Context, req *models.Subject) error
+	GetSubjectList(ctx context.Context, req *models.GetSubjectsList) (*models.GetSubjectsList, error)
+	GetSubjectByID(ctx context.Context, id string) (*models.Subject, error)
+	UpdateSubject(ctx context.Context, req *models.Subject) error
+	DeleteSubject(ctx context.Context, id string) error
+}
+
+type StudentRepoI interface {
+	CreateStudent(ctx context.Context, req *models.Student) error
+	GetStudentList(ctx context.Context, req *models.GetStudentsList) (*models.GetStudentsList, error)
+	GetStudentByID(ctx context.Context, id string) (*models.Student, error)
+	UpdateStudent(ctx context.Context, req *models.Student) error
+	DeleteStudent(ctx context.Context, id string) error
+}
+
+type GradeRepoI interface {
+	CreateGrade(ctx context.Context, req *models.Grade) error
+	GetGradeList(ctx context.Context, req *models.GetGradesList) (*models.GetGradesList, error)
+	GetGradeByID(ctx context.Context, id string) (*models.Grade, error)
+	UpdateGrade(ctx context.Context, req *models.Grade) error
+	DeleteGrade(ctx context.Context, id string) error
+}
