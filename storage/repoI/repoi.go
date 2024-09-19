6bb766263bc5fe893efba224537f85ae5374993f
temur shamshidinov <temur.software.dev.go@gmail.com>
2024-09-19 17:11:37 +0500

    repoi/repoi.go

--- storage/repoI/repoi.go
+++ storage/repoI/repoi.go
@@ -16,7 +16,7 @@ type TeacherRepoI interface {
 
 type CourseRepoI interface {
 	CreateCourse(ctx context.Context, req *models.Course) error
-	GetCourseList(ctx context.Context, req *models.GetCoursesList) (*models.GetCoursesList, error)
+	GetCourseList(ctx context.Context, req *models.GetListReq) (*models.GetCoursesList, error)
 	GetCourseByID(ctx context.Context, id string) (*models.Course, error)
 	UpdateCourse(ctx context.Context, req *models.Course) error
 	DeleteCourse(ctx context.Context, id string) error
@@ -24,7 +24,7 @@ type CourseRepoI interface {
 
 type GroupRepoI interface {
 	CreateGroup(ctx context.Context, req *models.Group) error
-	GetGroupList(ctx context.Context, req *models.GetGroupsList) (*models.GetGroupsList, error)
+	GetGroupList(ctx context.Context, req *models.GetListReq) (*models.GetGroupsList, error)
 	GetGroupByID(ctx context.Context, id string) (*models.Group, error)
 	UpdateGroup(ctx context.Context, req *models.Group) error
 	DeleteGroup(ctx context.Context, id string) error
@@ -32,7 +32,7 @@ type GroupRepoI interface {
 
 type SubjectRepoI interface {
 	CreateSubject(ctx context.Context, req *models.Subject) error
-	GetSubjectList(ctx context.Context, req *models.GetSubjectsList) (*models.GetSubjectsList, error)
+	GetSubjectList(ctx context.Context, req *models.GetListReq) (*models.GetSubjectsList, error)
 	GetSubjectByID(ctx context.Context, id string) (*models.Subject, error)
 	UpdateSubject(ctx context.Context, req *models.Subject) error
 	DeleteSubject(ctx context.Context, id string) error
@@ -40,7 +40,7 @@ type SubjectRepoI interface {
 
 type StudentRepoI interface {
 	CreateStudent(ctx context.Context, req *models.Student) error
-	GetStudentList(ctx context.Context, req *models.GetStudentsList) (*models.GetStudentsList, error)
+	GetStudentList(ctx context.Context, req *models.GetListReq) (*models.GetStudentsList, error)
 	GetStudentByID(ctx context.Context, id string) (*models.Student, error)
 	UpdateStudent(ctx context.Context, req *models.Student) error
 	DeleteStudent(ctx context.Context, id string) error
@@ -48,7 +48,7 @@ type StudentRepoI interface {
 
 type GradeRepoI interface {
 	CreateGrade(ctx context.Context, req *models.Grade) error
-	GetGradeList(ctx context.Context, req *models.GetGradesList) (*models.GetGradesList, error)
+	GetGradeList(ctx context.Context, req *models.GetListReq) (*models.GetGradesList, error)
 	GetGradeByID(ctx context.Context, id string) (*models.Grade, error)
 	UpdateGrade(ctx context.Context, req *models.Grade) error
 	DeleteGrade(ctx context.Context, id string) error
