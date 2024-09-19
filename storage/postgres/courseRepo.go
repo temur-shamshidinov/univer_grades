b00db7c86641f454ed3bc303564fe54c1ba6e6b8
temur shamshidinov <temur.software.dev.go@gmail.com>
2024-09-19 18:44:10 +0500

    create course

--- storage/postgres/courseRepo.go
+++ storage/postgres/courseRepo.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"context"
+	"log"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/temur-shamshidinov/univer_grades/models"
@@ -17,11 +18,34 @@ func NewCourseRepo(db *pgx.Conn) repoi.CourseRepoI {
 }
 
 func (c *courseRepo) CreateCourse(ctx context.Context, req *models.Course) error {
-	 return nil
+
+	query := `
+		INSERT INTO courses(
+			course_id,
+			course_name,
+			created_at,
+			updated_at
+		) VALUES (
+			$1, $2, $3, $4 
+		)
+	`
+	_, err := c.conn.Exec(
+		ctx, query,
+		req.CourseID,
+		req.CourseName,
+		req.CreatedAt,
+		req.UpdateAt,
+	)
+	if err != nil {
+		log.Println("Error with Create Course", err)
+		return err
+	}
+
+	return nil
 }
 
 func (c *courseRepo) GetCourseList(ctx context.Context, req *models.GetListReq) (*models.GetCoursesList, error) {
-	return nil, nil 
+	return nil, nil
 }
 
 func (c *courseRepo) GetCourseByID(ctx context.Context, id string) (*models.Course, error) {
