ce4e3d9b8281a2664596b0d01349979b222fa3c2
temur shamshidinov <temur.software.dev.go@gmail.com>
2024-09-19 15:29:44 +0500

    pkg/database/psql.go

--- go.mod
+++ go.mod
@@ -3,6 +3,14 @@ module github.com/temur-shamshidinov/univer_grades
 go 1.22.5
 
 require (
+	github.com/jackc/pgx/v5 v5.7.1
 	github.com/joho/godotenv v1.5.1
 	github.com/spf13/cast v1.7.0
+)
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 )
---
+++ pkg/database/psql.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/temur-shamshidinov/univer_grades/config"
+)
+
+func ConnectToDb(pgCfg config.Config) (*pgx.Conn, error) {
+
+	var ctx = context.Background()
+
+	url := fmt.Sprintf(
+		"postgresql://%s:%s@%s:%d/%s",
+		pgCfg.PgConfig.Username,
+		pgCfg.PgConfig.Password,
+		pgCfg.PgConfig.Host,
+		pgCfg.PgConfig.Port,
+		pgCfg.PgConfig.DatabaseName,
+	)
+
+	conn, err := pgx.Connect(ctx,url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v \n", err)
+		return nil, err
+	}
+
+	return conn, nil
+}
