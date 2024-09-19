b8cbc0eace1eb071d71f8acbae275aab4a4f12a1
temur shamshidinov <temur.software.dev.go@gmail.com>
2024-09-19 16:57:25 +0500

    repoI/repoi.go CourseRepoI

--- storage/repoI/repoi.go
+++ storage/repoI/repoi.go
@@ -6,10 +6,20 @@ import (
 	"github.com/temur-shamshidinov/univer_grades/models"
 )
 
-type TeacherRepoI interface{
+type TeacherRepoI interface {
 	CreateTeacher(ctx context.Context, req *models.Teacher) error
 	GetTeacherList(ctx context.Context, req *models.GetListReq) (*models.GetTeacherList, error)
 	GetTeacherByID(ctx context.Context, id string) (*models.Teacher, error)
 	UpadeTeacher(ctx context.Context, req *models.Teacher) error
 	DeletedTeacher(ctx context.Context, id string) error
 }
+
+type CourseRepoI interface {
+	CreateCourse(ctx context.Context, req *models.Course) error
+	GetCourseList(ctx context.Context, req *models.GetCoursesList) (*models.GetCoursesList, error)
+	GetCourseByID(ctx context.Context, id string) (*models.Course, error)
+	UpdateCourse(ctx context.Context, req *models.Course) error
+	DeleteCourse(ctx context.Context, id string) error
+}
+
+
